96b7ce6f78a00c9954be14f84888b37595cbcd10
Yulia Kuderko <yulia2232@gmail.com>
2022-06-19 01:18:40 +0300

    Rollback of mistake in ex4

--- src/ex4/ItemManager.js
+++ src/ex4/ItemManager.js
@@ -1,20 +1,33 @@
 import PokemonClient from "./PokemonClient.js";
-import item from "./models/item.js"
+import fs from 'fs';
 const filename = './data.json';
 
 class ItemManager {
     constructor(element) {
+        this.load();
         this.PokemonClient = new PokemonClient();
+    }
+
+    load() {
+        const data = fs.readFileSync(filename, { encoding: 'utf8', flag: 'r' });
+        this.todoList = JSON.parse(data);
+        if (this.todoList.length === 0) {
+            this.lastID = 0;
+            return;
+        }
+        this.lastID = Math.max(...Object.keys(this.todoList), 0);
+    }
+
+    store() {
+        fs.writeFileSync(filename, JSON.stringify(this.todoList));
     }
 
     getItems() {
         return this.todoList;
     }
-
     async addTask(itemText) {
         let itemTexts = [];
         const split = itemText.split(`,`);
-
         if (parseInt(itemText)) {
             itemTexts = await this.PokemonClient.fetchPokemon([parseInt(itemText)]);
         }
@@ -30,24 +43,19 @@ class ItemManager {
         })
         this.store();
     }
-
     removeTask(id) {
         delete this.todoList[id];
         this.store();
     }
-
     setDone(id) {
         this.todoList[id].done = true;
     }
-
     setUnDone(id) {
         this.todoList[id].done = false;
     }
-
     get doneTasks() {
         return this.todoList.values().filter(item => item.done).length;
     }
-
     clear() {
         this.todoList = {};
         this.store();
